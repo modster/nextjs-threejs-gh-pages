cfe74fb16e55beaf25d831d8cfe4e006f1200470
E. M. Greeff <unmodster@gmail.com>
2022-04-06 08:01:34 -0600

    Deploy Next.js to gh-pages

--- out/_next/static/hwMrSTIIKUJb5Y0yOZ-H4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b45dc8fa3dbf2926.js"],"/_error":["static/chunks/pages/_error-5b31083fea36d153.js"],"/about":["static/chunks/pages/about-0976bcf83e3e86dc.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
