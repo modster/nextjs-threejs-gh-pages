d12c9a09cccfc45b0b4885e499705221c8db5a51
E. M. Greeff <unmodster@gmail.com>
2022-04-06 12:03:54 -0600

    Updates

---
+++ _next/static/9qqR7R-D2AZ_PDvK1pkz-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-383251aeadbee6d8.js"],"/_error":["static/chunks/pages/_error-5b31083fea36d153.js"],"/about":["static/chunks/pages/about-7f604e28e36f366a.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
